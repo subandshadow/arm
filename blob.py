39e928600e0e10668fa2910606e4f3403f604c71
Mark Davidov <106958072+CR-MarkDavidov@users.noreply.github.com>
2023-06-25 17:44:33 +0300

    py

---
+++ blob.py
@@ -0,0 +1,63 @@
+import os, uuid
+from azure.identity import DefaultAzureCredential
+from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+
+connection_string = os.getenv('AZURE_STORAGE_CONNECTION_STRING')
+
+try:
+    # Create the BlobServiceClient object
+    blob_service_client = BlobServiceClient.from_connection_string(connection_string)
+
+    # Create a unique name for the container
+    container_name = str(uuid.uuid4())
+
+    # Create the container
+    container_client = blob_service_client.create_container(container_name)
+
+    # Create a local directory to hold blob data
+    local_path = "./data"
+    # os.mkdir(local_path)
+
+    for i in range(10):
+    # Create a file in the local data directory to upload and download
+        local_file_name = str(uuid.uuid4()) + "_" + str(i) + ".txt"
+        upload_file_path = os.path.join(local_path, local_file_name)
+
+        # Create a blob client using the local file name as the name for the blob
+        blob_client = blob_service_client.get_blob_client(container=container_name, blob=local_file_name)
+
+        # Write text to the file
+        file = open(file=upload_file_path, mode='w')
+        file.write("Hello, World!")
+        file.close()
+
+        print("\nUploading to Azure Storage as blob:\n\t" + local_file_name)
+
+        # Upload the created file
+        with open(file=upload_file_path, mode="rb") as data:
+            blob_client.upload_blob(data)
+
+    print("\nListing blobs...")
+
+    # List the blobs in the container
+    blob_list = container_client.list_blobs()
+    for blob in blob_list:
+        print("\t" + blob.name)
+
+    # # Clean up
+    # print("\nPress the Enter key to begin clean up")
+    # input()
+
+    # print("Deleting blob container...")
+    # container_client.delete_container()
+
+    # print("Deleting the local source and downloaded files...")
+    # os.remove(upload_file_path)
+    
+    # # os.rmdir(local_path)
+
+    print("Done")
+
+except Exception as ex:
+    print('Exception:')
+    print(ex)
